9efffb761d5694401ab06c642fedfbd8894b83db
Ubuntu <ubuntu@ip-172-31-37-47.ap-northeast-1.compute.internal>
2021-03-05 11:48:02 +0000

    ans

--- task.rb
+++ task.rb
@@ -157,12 +157,13 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-  keys = []
-  data.each_key do |key|
-    keys << key
-  end
-  p keys
+  # keys = []
+  # data.each_key do |key|
+  #   keys << key
+  # end
+  # p keys
   
+  p data.keys
 end
 
 def q15
@@ -170,17 +171,20 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.has_key?(:age) 
-    puts "OK"
-  else
-    puts "NG"
-  end
   
-  if data2.has_key?(:age) 
-    puts "OK"
-  else
-    puts "NG"
-  end
+  # if data1.has_key?(:age) 
+  #   puts "OK"
+  # else
+  #   puts "NG"
+  # end
+  
+  # if data2.has_key?(:age) 
+  #   puts "OK"
+  # else
+  #   puts "NG"
+  # end
+puts  data1.has_key?(:age) ? "OK" : "NG"
+puts  data2.has_key?(:age) ? "OK" : "NG"
 
 end
 
