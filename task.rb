62e4549b0222b5b9de0cd0a0e9543be08e3525c4
Ubuntu <ubuntu@ip-172-31-37-47.ap-northeast-1.compute.internal>
2021-04-04 11:11:32 +0000

    q17~19に解答

--- task.rb
+++ task.rb
@@ -205,7 +205,22 @@ end
 
 class UserQ17
   # 以下に回答を記載
-
+  attr_reader :name, :age, :gender
+  
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
+    @gender = params[:gender]
+  end
+  
+  def info
+    puts <<~TEXT
+    名前：#{@name}
+    年齢：#{@age}
+    性別：#{@gender}
+    TEXT
+  end
+  
 end
 
 def q17
@@ -220,7 +235,23 @@ end
 
 class UserQ18
   # 以下に回答を記載
-
+ attr_reader :name, :age
+ 
+ def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
+ end
+ 
+ def introduce
+   
+   if name == "あじー"
+     puts "こんにちは，あじーと申します。宜しくお願いいたします。"
+    else
+     puts "はいさいまいど〜，ゆたぼんです！！！"
+    end
+    
+ end
+ 
 end
 
 def q18
@@ -234,9 +265,9 @@ end
 
 class Item
   # 以下を修正して下さい
-
+  attr_reader :name
   def initialize(name)
-    @name = name
+   @name = name.values
   end
 end
 
@@ -248,7 +279,13 @@ end
 
 class UserQ20
   # 以下に回答を記載
-
+attr_reader :name, :age
+  
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
+  end
+  
 end
 
 class Zoo
